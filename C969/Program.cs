a1fecb15e143f7e49e4f3c28f6d307dff6f6de34
jdun28 <jencdunlap@gmail.com>
2020-07-03 09:53:10 -0700

    Removing saved DB connection info from program.cs

--- C969/Program.cs
+++ C969/Program.cs
@@ -8,12 +8,6 @@ namespace ScheduleProgram
 {
     static class Program
     {
-
-        /* Connection String
-           Server name: 3.227.166.251
-           Database name: U06EtK
-           Username: U06EtK
-           Password: 53688740958 */
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
